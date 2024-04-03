115782cc50e39e35156a85b6430c84a349dc1a37
amoobarsam <141170967+amoobarsam@users.noreply.github.com>
2024-04-03 20:29:24 +0330

    complete
    complete

--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('' , views.store ,name= 'store'),
     path('checkout/' , views.checkout , name= 'checkout'),
     path('cart/' , views.cart , name= 'cart' ),
-    path('update_item/' , views.updateitem , name= 'update_item' ),
+    path('update_item/' , views.updateItem , name= 'update_item' ),
+    path('process_order/' , views.processOrder , name= 'process_order' ),
 
     
     
